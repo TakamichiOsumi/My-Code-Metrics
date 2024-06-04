435cde61f86d8563b4b31848f6f557ea02007bea
Takamichi Osumi <takamichiosumi01@gmail.com>
2024-06-04 21:13:52 +0900

    Make ruby one-liner dump the calculation process.

--- src/my_initial_stats.sh
+++ src/my_initial_stats.sh
@@ -6,4 +6,4 @@ file_name=stats_`date +%Y%m%d`.txt
 wc -l `find ./../repos -name \*.h -or -name \*.c -or -name \*.md -or -name Makefile` > $file_name
 
 # Do the double check by one-liner program.
-sed '$d' $file_name | ruby -e "total = 0; ARGF.each do |line| splited = line.chomp.split(' '); total = total + splited[0].to_i; end; puts total;"
+sed '$d' $file_name | ruby -e "total = 0; ARGF.each do |line| splited = line.chomp.split(' '); STDERR.puts \"#{splited[1]}, #{total}, #{splited[0]}\"; total = total + splited[0].to_i;; end; puts total;"
