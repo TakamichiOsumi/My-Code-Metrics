e2341b981ce93842d700af780aa3ed16fa097120
Takamichi Osumi <takamichiosumi01@gmail.com>
2024-06-05 10:38:24 +0900

    Modified typos.

--- init.sh
+++ init.sh
@@ -6,4 +6,4 @@ git submodule init
 echo "Execute 'git submodule update'"
 git submodule update
 
-echo "Now, only unique parts of each repository have been downloaded, without any submodules redundancy."
+echo "Downloaded unique parts of each repository, without any submodules redundancy."
--- src/my_initial_stats.sh
+++ src/my_initial_stats.sh
@@ -2,8 +2,8 @@
 
 file_name=stats_`date +%Y%m%d`.txt
 
-# Print line numbres of all files
+# Print line numbers of all files
 wc -l `find ./../repos -name \*.h -or -name \*.c -or -name \*.md -or -name Makefile` > $file_name
 
 # Do the double check by one-liner program.
-sed '$d' $file_name | ruby -e "total = 0; ARGF.each do |line| splited = line.chomp.split(' '); STDERR.puts \"#{splited[1]}, #{total}, #{splited[0]}\"; total = total + splited[0].to_i;; end; puts total;"
+sed '$d' $file_name | ruby -e "total = 0; ARGF.each do |line| splited = line.chomp.split(' '); STDERR.puts \"#{splited[1]}, #{total}, #{splited[0]}\"; total = total + splited[0].to_i; end; puts total;"
