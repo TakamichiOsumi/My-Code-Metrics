1a85f39b8c8bd39a16c28f71a4aeea5393534f1b
Takamichi Osumi <takamichiosumi01@gmail.com>
2024-07-01 13:30:42 +0900

    Minor comment fix.

--- init.sh
+++ init.sh
@@ -6,4 +6,4 @@ git submodule init
 echo "Execute 'git submodule update'"
 git submodule update
 
-echo "Downloaded unique parts of each repository, without any submodules redundancy."
+echo "Got all repositories without any submodule redundancy"
